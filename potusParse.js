f84ed5bb53507481f469f7763850ea810b6b3dee
Nina Johnson <9makesthings@gmail.com>
2019-09-03 17:09:35 -0500

    importing potusParse into potusScraper and retrieving all potus names and bdays

--- potusParse.js
+++ potusParse.js
@@ -1,12 +1,17 @@
 const rp = require('request-promise');
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const $ = require('cheerio');
 
-rp(url)
-    .then(function(html) {
-        console.log(  $('.firstheading', html).text() );
-        console.log( $('.bday', html).text() );
-        
-    })
-    .catch(function(error) {
-        console.log( `Something didn't work. Try again later.` );
-    })
+const potusParse = function(url) {
+    return rp(url)
+        .then(function(html) {
+            return {
+                name: $('.firstHeading', html).text(),
+                birthday: $('.bday', html).text()
+            };
+        })
+        .catch(function(error) {
+            console.log( `Something didn't work. Try again later.` );
+        })
+};
+
+module.exports = potusParse;
--- potusScraper.js
+++ potusScraper.js
@@ -1,5 +1,6 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
+const potusParse = require('./potusParse');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 rp(url)
@@ -9,9 +10,16 @@ rp(url)
         for( let i = 0; i < 45; i++ ){
             wikiUrls.push( $('big > a', html)[i].attribs.href );
         }
-        console.log( wikiUrls );
+        
+        return Promise.all(
+            wikiUrls.map(function(url) {
+                return potusParse('https://en.wikipedia.org' + url);
+            })
+        )
+    })
+    .then(function(presidents) {
+        console.log( presidents );
     })
     .catch(function(error) {
-        console.log( `Caught an error.` );
-        
+        console.log( `Caught an error.`, error );
     });
